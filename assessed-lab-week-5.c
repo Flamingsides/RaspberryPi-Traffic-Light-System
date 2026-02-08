454b564c45908b705de20e9f23237899c420b9ca
Suhaib Hameed Zuberi <sz2056@hw.ac.uk>
2026-02-09 00:02:29 +0800

    Bug fixes needed

--- assessed-lab-week-5.c
+++ assessed-lab-week-5.c
@@ -5,7 +5,7 @@
 #include <sys/mman.h>   // mmap()
 
 // GPIO base address for RasberryPi 4
-#define GPIO_BASE 0xFE200000
+#define GPIO_BASE 0xFE200000UL
 
 // GPIO block size is 4kB to be memory mapped
 #define GPIO_BLOCK_SIZE 4096
@@ -27,9 +27,10 @@ unsigned short getGpio();
 
 int main(int argc, char *argv[]) {
     // Getting an address to the device memory mapped to gpio
-    volatile uint32_t *gpio;
     printf("LOG: Getting gpio\n");
-    unsigned short result = getGpio(&gpio);
+    volatile uint32_t *gpio;
+    unsigned short result = getGpio(gpio);
+
     switch (result) {
         case 1:
             fprintf(stderr, "ERR: Unable to open \"/dev/mem\"\nAre you root?\n");
@@ -41,10 +42,13 @@ int main(int argc, char *argv[]) {
     
     if (result != 0) return result;
 
-    printf("LOG: Setting mode for each pin");
-    // Blink red light
+    printf("LOG: Setting mode for each pin\n");
+    
+    // Set output mode forGPIO pins 10, 11, and 13
+    printf("*(gpio+1): %b", *(gpio + GPFSEL_OFF));
     *(gpio + GPFSEL_OFF) = 0b001000001001;
 
+    // Blink red light
     while (1) {
         // Set RED_LED (10) to high
         *(gpio + GPSET_OFF) = 0b1;
@@ -53,7 +57,10 @@ int main(int argc, char *argv[]) {
         // Reference: https://pubs.opengroup.org/onlinepubs/009696899/functions/usleep.html
         usleep(DELAY);
         
+        // Clear RED_LED (set to low)
         *(gpio + GPCLR_OFF) = 0b1;
+
+        // Delay
         usleep(DELAY);
     }
     
@@ -62,23 +69,23 @@ int main(int argc, char *argv[]) {
 
 // Function to memory-map "/dev/mem" to an gpio
 unsigned short getGpio(volatile uint32_t *gpio) {
-    printf("LOG: Opening \"/dev/mem\" now");
+    printf("LOG: Opening \"/dev/gpiomem\" now\n");
     // Opening the /dev/memory device to map it to gpio
     int fileDescriptor = open(
-        "/dev/mem", // Address where memory exists
+        "/dev/gpiomem", // Address where memory exists
         O_RDWR | O_SYNC | O_CLOEXEC // Flags: Read/Write mode + syncronised data access + close upon execution
     );
 
-    printf("LOG: fileDescriptor: %d", fileDescriptor);
+    printf("LOG: fileDescriptor: %d\n", fileDescriptor);
     // If there is an error opening the file descriptor
     if (fileDescriptor < 0)     
         return 1;
     
     // Mapping the /dev/mem to gpio
     gpio = (volatile uint32_t *) mmap(
-        NULL,                   // Let any memory location chosen
+        0,                     // Let any memory location chosen
         GPIO_BLOCK_SIZE,        // Size of memory to map
-        PROT_WRITE | PROT_READ, // Writing and reading protection
+        PROT_READ | PROT_WRITE, // Writing and reading protection
         MAP_SHARED,             // Shared mapping
         fileDescriptor,         // Where to read data from
         GPIO_BASE               // The base GPIO address
@@ -86,6 +93,7 @@ unsigned short getGpio(volatile uint32_t *gpio) {
 
     // In case of error mapping gpio memory
     if (gpio == MAP_FAILED) {
+        perror("mmap");
         close(fileDescriptor);
         return 2;
     }
---
+++ makefile
@@ -0,0 +1,2 @@
+assessed-lab-week-5: assessed-lab-week-5.c
+	gcc -o assessed-lab-week-5 -Wall -g assessed-lab-week-5.c
